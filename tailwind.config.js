2dc5fc982688b6e34443dbd787148eda5981f324
최유진 <yjyj0774@gmail.com>
2024-10-22 19:35:58 +0900

    tailwind config 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 모든 .js, .jsx, .ts, .tsx 파일
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
