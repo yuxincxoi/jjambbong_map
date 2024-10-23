ad0e5073f9110754cde5d0d9c9736a10365e3f17
최유진 <yjyj0774@gmail.com>
2024-10-23 11:04:35 +0900

    tailwindCSS로 파일명 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../../style/index.css";
+import "../tailwind.css";
 import Map from "../components/Map";
 
 const App: React.FC = () => {
