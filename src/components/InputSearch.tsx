dae2e9971fcdbf264f03a30413fdeac2de334224
최유진 <yjyj0774@gmail.com>
2024-11-06 16:42:34 +0900

    검색어 상태 감지

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 const InputSearch = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value);
+  };
+
   return (
     <>
       <div className="flex justify-center mt-6">
@@ -10,6 +14,7 @@ const InputSearch = () => {
           type="text"
           placeholder="대전 짬뽕"
           value={searchKeyword}
+          onChange={handleInputChange}
           className="w-[40%] h-10 border-2 px-4 rounded-2xl hover:border-main-color focus:outline-none focus:border-main-color"
         />
       </div>
