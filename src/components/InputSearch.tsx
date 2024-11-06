3ddac9e592448819d569be2ae1041778f71ec2f2
최유진 <yjyj0774@gmail.com>
2024-11-06 16:33:36 +0900

    검색창 디자인

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,5 +1,15 @@
 const InputSearch = () => {
-  return <></>;
+  return (
+    <>
+      <div className="flex justify-center mt-6">
+        <input
+          type="text"
+          placeholder="대전 짬뽕"
+          className="w-[40%] h-10 border-2 px-4 rounded-2xl hover:border-main-color focus:outline-none focus:border-main-color"
+        />
+      </div>
+    </>
+  );
 };
 
 export default InputSearch;
