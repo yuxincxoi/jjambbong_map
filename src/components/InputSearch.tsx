64f8855ebb65ee09ad09be74360a0c9a08927876
최유진 <yjyj0774@gmail.com>
2024-11-06 16:37:45 +0900

    검색어 변수 설정

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 const InputSearch = () => {
+  const [searchKeyword, setSearchKeyword] = useState("");
+
   return (
     <>
       <div className="flex justify-center mt-6">
         <input
           type="text"
           placeholder="대전 짬뽕"
+          value={searchKeyword}
           className="w-[40%] h-10 border-2 px-4 rounded-2xl hover:border-main-color focus:outline-none focus:border-main-color"
         />
       </div>
