edcfa14e46b796b8089aa857e0d02fe3a7442b95
최유진 <yjyj0774@gmail.com>
2024-11-06 17:26:49 +0900

    EachPlace 컴포넌트 생성

--- src/components/main/placeListTable/PlaceListTable.tsx
+++ src/components/main/placeListTable/PlaceListTable.tsx
@@ -1,8 +1,12 @@
+import EachPlace from "./eachPlace/EachPlace";
+
 const PlaceListTable = () => {
   return (
     <>
       <div className="flex justify-center mt-6">
-        <div className="bg-[#F8F8F8] w-[50%]">Table</div>
+        <div className="bg-[#F8F8F8] w-[50%]">
+          <EachPlace />
+        </div>
       </div>
     </>
   );
---
+++ src/components/main/placeListTable/eachPlace/EachPlace.tsx
@@ -0,0 +1,15 @@
+const EachPlace = () => {
+  return (
+    <>
+      <div className="bg-white w-[99%] m-1">
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default EachPlace;
