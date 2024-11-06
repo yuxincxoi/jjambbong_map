4c305d13828700777539c9a7b75bcdbd51813609
최유진 <yjyj0774@gmail.com>
2024-11-06 16:44:52 +0900

    enter를 이용한 검색기능 추가

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { searchPlace } from "../modules/map/searchPlace";
 
 const InputSearch = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      searchPlace(searchKeyword);
+    }
   };
 
   return (
@@ -15,6 +22,7 @@ const InputSearch = () => {
           placeholder="대전 짬뽕"
           value={searchKeyword}
           onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
           className="w-[40%] h-10 border-2 px-4 rounded-2xl hover:border-main-color focus:outline-none focus:border-main-color"
         />
       </div>
