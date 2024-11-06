0d57e35cb7f066869902361e193d929c175b363a
최유진 <yjyj0774@gmail.com>
2024-11-06 17:36:20 +0900

    EachPlace 컴포넌트 디자인

--- src/components/main/placeListTable/eachPlace/EachPlace.tsx
+++ src/components/main/placeListTable/eachPlace/EachPlace.tsx
@@ -1,12 +1,12 @@
 const EachPlace = () => {
   return (
     <>
-      <div className="bg-white w-[99%] m-1">
-        <div>
-          <div></div>
-          <div></div>
+      <div className="bg-white w-[99%] m-1 flex  justify-between">
+        <div className="px-5 py-3">
+          <div className="text-2xl font-semibold">짬뽕집</div>
+          <div>대전광역시 서구 가나다로123번길 456 1층</div>
         </div>
-        <div></div>
+        <div className="w-14 h-14 mt-3 bg-main-color"></div>
       </div>
     </>
   );
