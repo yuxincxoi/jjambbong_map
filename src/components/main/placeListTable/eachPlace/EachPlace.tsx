93c553e7562ae5e5ff551aee5d501230b4f3f894
최유진 <yjyj0774@gmail.com>
2024-11-07 13:41:25 +0900

    검색결과 받아오기

--- src/components/main/placeListTable/eachPlace/EachPlace.tsx
+++ src/components/main/placeListTable/eachPlace/EachPlace.tsx
@@ -5,8 +5,8 @@ const EachPlace: React.FC<EachPlaceProps> = ({ placeName, address }) => {
     <>
       <div className="bg-white w-[99%] m-1 flex  justify-between">
         <div className="px-5 py-3">
-          <div className="text-2xl font-semibold">짬뽕집</div>
-          <div>대전광역시 서구 가나다로123번길 456 1층</div>
+          <div className="text-2xl font-semibold">{placeName}</div>
+          <div>{address}</div>
         </div>
         <div className="w-14 h-14 mt-3 bg-main-color"></div>
       </div>
