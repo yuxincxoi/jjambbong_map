7e948e648c9847f1658d1ca373be9727057aa1a1
최유진 <yjyj0774@gmail.com>
2024-11-07 15:55:05 +0900

    검색된 place 표시하기

--- src/components/main/placeListTable/PlaceListTable.tsx
+++ src/components/main/placeListTable/PlaceListTable.tsx
@@ -9,7 +9,13 @@ const PlaceListTable = () => {
     <>
       <div className="flex justify-center mt-6">
         <div className="bg-[#F8F8F8] w-[50%]">
-          <EachPlace />
+          {places.length > 0 ? (
+            places.map((place) => (
+              <EachPlace key={place.placeName} place={place} />
+            ))
+          ) : (
+            <p>검색된 장소가 없습니다.</p>
+          )}
         </div>
       </div>
     </>
