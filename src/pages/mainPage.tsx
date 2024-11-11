c26791e932f9ab66a6a9c1db6ff842e7e3824e6d
최유진 <yjyj0774@gmail.com>
2024-11-11 21:19:24 +0900

    버튼 추가

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,6 +1,7 @@
 import Map from "../components/main/Map";
 import Title from "../components/Title";
 import PlaceListTable from "../components/main/placeListTable/PlaceListTable";
+import { Link } from "react-router-dom";
 
 const MainPage: React.FC = () => {
   return (
@@ -9,6 +10,15 @@ const MainPage: React.FC = () => {
       <Title page="wka" />
       <Map />
       <PlaceListTable />
+      <Link to="/">
+        <button>Go to Home Page</button>
+      </Link>
+      <Link to="/myPage">
+        <button>Go to My Page</button>
+      </Link>
+      <Link to="/likePage">
+        <button>Go to Like Page</button>
+      </Link>
     </>
   );
 };
