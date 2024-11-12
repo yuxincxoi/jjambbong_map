afe1be45d04721901f57325696bbef6b7f8b4e34
최유진 <yjyj0774@gmail.com>
2024-11-12 14:31:43 +0900

    pathname에 따라 title 색 변경

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,18 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import TitleProps from "../interfaces/components/Title.interface";
 
 const Title: React.FC = () => {
+  const [title, setTitle] = useState<JSX.Element | string>("I LIKE 짬뽕");
+
   useEffect(() => {
-    const title = document.getElementById("title");
-    if (page === "wka" && title) {
-      title.style.backgroundColor = "blue";
+    const page = window.location.pathname.split("/").pop();
+    let newTitle: JSX.Element | string = "I LIKE 짬뽕";
+
+    if (page === "likePage" && title) {
+      newTitle = (
+        <>
+          I <span style={{ color: "red" }}>LIKE</span> 짬뽕
+        </>
+      );
+    } else if (page === "myPage" && title) {
+      newTitle = (
+        <>
+          <span style={{ color: "red" }}>I</span> LIKE 짬뽕
+        </>
+      );
+    } else if (title) {
+      newTitle = (
+        <>
+          I LIKE <span style={{ color: "red" }}>짬뽕</span>
+        </>
+      );
     }
-  }, [page]); // page 값이 변경될 때마다 실행
+
+    setTitle(newTitle);
+  }, [window.location.pathname]);
 
   return (
     <>
       <div>
-        <h1 id="title">I LIKE 짬뽕</h1>
+        <h1>{title}</h1>
       </div>
     </>
   );
