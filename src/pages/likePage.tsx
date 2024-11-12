9aaa1acb1bb2ed98ebfca560ea90abd29bbc6f3a
최유진 <yjyj0774@gmail.com>
2024-11-12 14:56:19 +0900

    좋아요페이지 컴포넌트 배치

--- src/pages/likePage.tsx
+++ src/pages/likePage.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Title from "../components/Title";
+import Map from "../components/main/Map";
+import PlaceListTable from "../components/main/placeListTable/PlaceListTable";
 
 export default function LikePage() {
   return (
     <div>
       <Title />
+      <Map />
+      <PlaceListTable />
       <Link to="/">
         <button className="px-2 py-1 bg-main-color text-white rounded mr-2 hover:border-main-color hover:text-main-color hover:bg-white">
           Go to Home Page
