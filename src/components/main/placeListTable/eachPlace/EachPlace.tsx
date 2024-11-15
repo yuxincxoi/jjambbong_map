f75a712610bb9b6dcfc7bf41c1830feb0a0cc4bf
최유진 <yjyj0774@gmail.com>
2024-11-15 17:17:38 +0900

    좋아요버튼 클릭 이벤트 구현

--- src/components/main/placeListTable/eachPlace/EachPlace.tsx
+++ src/components/main/placeListTable/eachPlace/EachPlace.tsx
@@ -1,6 +1,30 @@
 import EachPlaceProps from "../../../../interfaces/components/main/placeListTable/eachPlace/EachPlace.interface";
 
 const EachPlace: React.FC<EachPlaceProps> = ({ place }) => {
+  const heartFull = "url('./img/fullHeart.png')";
+  const heartEmpty = "url('./img/emptyHeart.png')";
+
+  let [isHeartOn, setIsHeartOn] = useState(false);
+  const heart = document.getElementById("heart");
+
+  heart?.addEventListener("click", () => {
+    if (isHeartOn) {
+      setIsHeartOn(false);
+      console.log(isHeartOn);
+    } else {
+      setIsHeartOn(true);
+      console.log(isHeartOn);
+    }
+  });
+
+  useEffect(() => {
+    if (heart && isHeartOn) {
+      heart.style.backgroundImage = heartFull;
+    } else if (heart && !isHeartOn) {
+      heart.style.backgroundImage = heartEmpty;
+    }
+  }, [isHeartOn]);
+
   return (
     <>
       <div className="bg-white w-[99%] m-1 flex justify-between">
