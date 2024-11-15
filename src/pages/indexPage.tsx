943fe427bcc438ff9eab1b9134f6fcc24f1df654
최유진 <yjyj0774@gmail.com>
2024-11-15 19:38:42 +0900

    login page button 추가

--- src/pages/indexPage.tsx
+++ src/pages/indexPage.tsx
@@ -2,24 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Title from "../components/Title";
 import Input from "../components/Input";
+import Button from "../components/Button";
 
 function IndexPage() {
   return (
-    <div>
+    <div className="mt-[200px]">
       <h1>Home Page</h1>
       <Title />
       <Input placeholder="ID" className="mt-4" />
       <Input placeholder="password" />
-      <Link to="/signupPage">
-        <button className="px-2 py-1 bg-main-color text-white rounded mr-2 hover:border-main-color hover:text-main-color hover:bg-white">
-          Go to Signup Page
-        </button>
-      </Link>
       <Link to="/mainPage">
-        <button className="px-2 py-1 bg-main-color text-white rounded mr-2 hover:border-main-color hover:text-main-color hover:bg-white">
-          Go to Main Page
-        </button>
+        <Button
+          buttonName="Sign in"
+          className="w-60 h-9 mt-4 px-2 py-1 bg-main-color text-white rounded-md hover:border-main-color hover:text-main-color hover:bg-white"
+        />
       </Link>
+      <div className="flex mt-8 justify-center text-sm">
+        <p className="text-[#909090] font-thin">Don't have an account ?</p>
+        <Link to="/signupPage">
+          <Button
+            buttonName="Sing up"
+            className="underline text-[#0570DA] hover:no-underline ml-3"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
