272b7f32df6210cb3c1cb48f32bfb087b78450b8
최유진 <yjyj0774@gmail.com>
2024-11-15 19:13:44 +0900

    login input 추가

--- src/pages/indexPage.tsx
+++ src/pages/indexPage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Title from "../components/Title";
+import Input from "../components/Input";
 
 function IndexPage() {
   return (
     <div>
       <h1>Home Page</h1>
       <Title />
+      <Input placeholder="ID" className="mt-4" />
+      <Input placeholder="password" />
       <Link to="/signupPage">
         <button className="px-2 py-1 bg-main-color text-white rounded mr-2 hover:border-main-color hover:text-main-color hover:bg-white">
           Go to Signup Page
