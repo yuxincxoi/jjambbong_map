467bb6743718e0b6734b13bd3c64d953ea5f22cb
최유진 <yjyj0774@gmail.com>
2024-11-15 19:13:22 +0900

    input 컴포넌트 생성

---
+++ src/components/Input.tsx
@@ -0,0 +1,20 @@
+interface InputProps {
+  placeholder: string;
+  className?: string; // optional className prop
+}
+
+const Input: React.FC<InputProps> = ({ placeholder, className }) => {
+  return (
+    <>
+      <div className="flex justify-center mt-2">
+        <input
+          type="text"
+          placeholder={placeholder}
+          className={`w-60 h-10 px-4 border-[1px] rounded-md hover:border-main-color focus:outline-none focus:border-main-color font-extralight ${className}`}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Input;
