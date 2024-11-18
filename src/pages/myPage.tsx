9f2a0941ea2ac3e807b0a2d8348827919b551d18
최유진 <yjyj0774@gmail.com>
2024-11-18 21:31:17 +0900

    input 추가

--- src/pages/myPage.tsx
+++ src/pages/myPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Title from "../components/Title";
+import Input from "../components/Input";
 
 export default function MyPage() {
   return (
     <div>
       <Title />
+      <Input placeholder="name" />
+      <Input placeholder="ID" />
+      <Input placeholder="password" />
+      <Input placeholder="confirm password" />
       <Link to="/mainPage">
         <button className="px-2 py-1 bg-main-color text-white rounded mr-2 hover:border-main-color hover:text-main-color hover:bg-white">
           Go to Main Page
