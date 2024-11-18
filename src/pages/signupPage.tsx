e71df4ada58c15df1c0f014ab3affedc47f42bb3
최유진 <yjyj0774@gmail.com>
2024-11-18 21:17:18 +0900

    input 추가

--- src/pages/signupPage.tsx
+++ src/pages/signupPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Title from "../components/Title";
+import Input from "../components/Input";
 
 export default function SignupPage() {
   return (
@@ -8,6 +9,10 @@ export default function SignupPage() {
       <h1>Signup Page</h1>
       <Title />
       <p>Create your Account</p>
+      <Input placeholder="name" />
+      <Input placeholder="ID" />
+      <Input placeholder="password" />
+      <Input placeholder="confirm password" />
       <Link to="/">
         <button className="px-2 py-1 bg-main-color text-white rounded mr-2 hover:border-main-color hover:text-main-color hover:bg-white">
           Go to Home Page
