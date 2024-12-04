eff5d77a811d85e4b05c4fc09fe9975b25f0d614
최유진 <yjyj0774@gmail.com>
2024-12-04 13:47:38 +0900

    스키마 및 모델 설정

---
+++ db/models/User.model.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { IUser } from "../interfaces/User.interface";
+
+const UserSchema = new mongoose.Schema<IUser>({
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  likePlace: [
+    {
+      place: { type: String, required: true },
+      address: { type: String },
+    },
+  ],
+});
+
+const User = mongoose.model<IUser>("User", UserSchema);
+
+export default User;
