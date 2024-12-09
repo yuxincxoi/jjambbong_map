7ed80ceaa6c8c3db61dcbdb7020e00b940fe5913
최유진 <yjyj0774@gmail.com>
2024-12-09 17:52:23 +0900

    에러 수정

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,15 +1,20 @@
 import User from "../../db/models/User.model";
+import { IUser } from "../../db/interfaces/User.interface";
 
 // 회원정보 수정
-export const updateUser = async (userId: string, updateData: any) => {
+export const updateUser = async (
+  userId: string,
+  updateData: Partial<IUser>
+) => {
   const user = await User.findById(userId);
   if (!user) {
     throw new Error("사용자를 찾을 수 없습니다.");
   }
 
-  // 업데이트 수행
   Object.keys(updateData).forEach((key) => {
-    // user[key] = updateData[key];
+    if (key in user) {
+      (user as any)[key] = updateData[key as keyof IUser];
+    }
   });
 
   return await user.save();
