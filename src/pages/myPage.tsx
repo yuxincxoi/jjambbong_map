e51d792e9d8280774ac2b6f2585942e8860404ab
최유진 <yjyj0774@gmail.com>
2024-12-09 17:27:37 +0900

    input 속성 설정

--- src/pages/myPage.tsx
+++ src/pages/myPage.tsx
@@ -20,10 +20,31 @@ export default function MyPage() {
   return (
     <div className="mt-[170px]">
       <Title />
-      <Input placeholder="name" className="mt-4" />
-      <Input placeholder="ID" />
-      <Input placeholder="password" />
-      <Input placeholder="confirm password" />
+      <Input
+        placeholder="name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        className="mt-4"
+      />
+      <Input
+        placeholder="ID"
+        name="ID"
+        value={formData.id}
+        onChange={handleChange}
+      />
+      <Input
+        placeholder="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <Input
+        placeholder="confirm password"
+        name="confirm password"
+        value={formData.confirmPassword}
+        onChange={handleChange}
+      />
       <Button
         buttonName="Save"
         className="w-60 h-9 mt-4 px-2 py-1 bg-main-color text-white rounded-md hover:border-main-color hover:text-main-color hover:bg-white"
