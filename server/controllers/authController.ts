2de121afc95010e3e6e4abc49459aa130d23a034
최유진 <yjyj0774@gmail.com>
2024-12-10 17:07:32 +0900

    토큰 생성

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -3,6 +3,11 @@ import User from "../../db/models/User.model";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
+// 토큰 생성 함수
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET as string);
+};
+
 // 로그인
 export const loginUser = async (req: Request, res: Response) => {
   const { id, password } = req.body;
@@ -22,6 +27,25 @@ export const loginUser = async (req: Request, res: Response) => {
       .status(401)
       .json({ message: "아이디 또는 비밀번호가 잘못되었습니다." });
   }
+
+  // 토큰 생성
+  const token = generateToken(user._id.toString());
+
+  // 쿠키에 토큰 설정
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" || false,
+    maxAge: 3600000,
+    sameSite: "strict",
+  });
+
+  res.status(200).json({
+    message: "로그인 성공",
+    user: {
+      id: user.id,
+      name: user.name,
+    },
+  });
 };
 
 // 회원가입
