1abb799784a517ec6321d977999b5b2959c7e32e
최유진 <yjyj0774@gmail.com>
2024-12-10 17:31:39 +0900

    인증 라우터 생성

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,27 +1,14 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/authController";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/authController";
 
 const router = express.Router();
 
-// 로그인
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const token = await loginUser(email, password);
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(400).json({ error: error });
-  }
-});
-
-// 회원가입
-router.post("/register", async (req, res) => {
-  try {
-    const newUser = await registerUser(req.body);
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ error: error });
-  }
-});
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 export default router;
