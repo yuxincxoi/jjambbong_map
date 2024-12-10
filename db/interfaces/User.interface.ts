71e14dfe3f8016b267a7592c2b844604a844b092
최유진 <yjyj0774@gmail.com>
2024-12-10 15:49:53 +0900

    User interface 수정

--- db/interfaces/User.interface.ts
+++ db/interfaces/User.interface.ts
@@ -2,6 +2,7 @@ import { ILikePlace } from "./LikePlace.interface";
 
 export interface IUser extends ILikePlace {
   name: string;
+  id: string;
   password: string;
   likePlace: ILikePlace[];
 }
--- db/models/User.model.ts
+++ db/models/User.model.ts
@@ -3,6 +3,7 @@ import { IUser } from "../interfaces/User.interface";
 
 const UserSchema = new mongoose.Schema<IUser>({
   name: { type: String, required: true },
+  id: { type: String, required: true },
   password: { type: String, required: true },
   likePlace: [
     {
