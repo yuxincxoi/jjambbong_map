4843cf8ec627ff65225856feaebedb19a4995e9d
최유진 <yjyj0774@gmail.com>
2024-12-10 16:03:36 +0900

    로그인 비밀번호 검증 로직 추가

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -14,6 +14,14 @@ export const loginUser = async (req: Request, res: Response) => {
       .status(401)
       .json({ message: "아이디 또는 비밀번호가 잘못되었습니다." });
   }
+
+  // 비밀번호 검증
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res
+      .status(401)
+      .json({ message: "아이디 또는 비밀번호가 잘못되었습니다." });
+  }
 };
 
 // 회원가입
