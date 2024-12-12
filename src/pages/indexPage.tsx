58770da91bd7237d9a9fc14fd09b6a198ba895b4
최유진 <yjyj0774@gmail.com>
2024-12-12 23:31:21 +0900

    form 태그 추가

--- src/pages/indexPage.tsx
+++ src/pages/indexPage.tsx
@@ -11,14 +11,16 @@ function IndexPage() {
   return (
     <div className="mt-[200px]">
       <Title />
-      <Input placeholder="ID" className="mt-4" />
-      <Input placeholder="password" />
-      <Link to="/mainPage">
-        <Button
-          buttonName="Sign in"
-          className="w-60 h-9 mt-4 px-2 py-1 bg-main-color text-white rounded-md hover:border-main-color hover:text-main-color hover:bg-white"
-        />
-      </Link>
+      <form>
+        <Input placeholder="ID" className="mt-4" />
+        <Input placeholder="password" />
+        <Link to="/mainPage">
+          <Button
+            buttonName="Sign in"
+            className="w-60 h-9 mt-4 px-2 py-1 bg-main-color text-white rounded-md hover:border-main-color hover:text-main-color hover:bg-white"
+          />
+        </Link>
+      </form>
       <div className="flex mt-8 justify-center text-sm">
         <p className="text-[#909090] font-thin">Don't have an account ?</p>
         <Link to="/signupPage">
