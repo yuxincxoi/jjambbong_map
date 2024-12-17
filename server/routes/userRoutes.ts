e4c8b41ffa50a487557748561e83d8a4fb8122b1
최유진 <yjyj0774@gmail.com>
2024-12-17 16:45:29 +0900

    컨트롤러 모듈화

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -4,14 +4,6 @@ import { updateUser } from "../controllers/userController";
 const router = express.Router();
 
 // 회원정보 수정
-router.put("/:id", async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const updatedUser = await updateUser(userId, req.body);
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
+router.put("/:id", updateUser);
 
 export default router;
