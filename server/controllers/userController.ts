3983050495e52a2f2cf8b96e66065215ecf6d249
최유진 <yjyj0774@gmail.com>
2024-12-17 16:44:15 +0900

    예외처리 구문 추가

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -5,32 +5,36 @@ import bcrypt from "bcryptjs";
 
 // 회원정보 수정
 export const updateUser = async (req: Request, res: Response) => {
-  const userId = req.params.id;
-  const { name, password } = req.body;
+  try {
+    const userId = req.params.id;
+    const { name, password } = req.body;
 
-  const user = await User.findOne({ id: userId });
-  if (!user) {
-    return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
+    const user = await User.findOne({ id: userId });
+    if (!user) {
+      return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
+    }
+
+    // 이름 업데이트
+    if (name) {
+      user.name = name;
+    }
+
+    // 비밀번호 해싱
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+    }
+
+    const updatedUser = await user.save();
+
+    return res.status(200).json({
+      message: "회원정보 수정 성공",
+      user: {
+        id: updatedUser.id,
+        name: updatedUser.name,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "회원정보 수정 중 오류 발생" });
   }
-
-  // 이름 업데이트
-  if (name) {
-    user.name = name;
-  }
-
-  // 비밀번호 해싱
-  if (password) {
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
-  }
-
-  const updatedUser = await user.save();
-
-  return res.status(200).json({
-    message: "회원정보 수정 성공",
-    user: {
-      id: updatedUser.id,
-      name: updatedUser.name,
-    },
-  });
 };
