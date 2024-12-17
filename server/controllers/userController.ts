587116aeb5f9cea41fd0af3fd81db5be10cf7432
최유진 <yjyj0774@gmail.com>
2024-12-17 16:42:59 +0900

    비밀번호 해싱하여 특정 필드만 업데이트하도록 수정

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,21 +1,36 @@
 import { Request, Response } from "express";
 import User from "../../db/models/User.model";
 import { IUser } from "../../db/interfaces/User.interface";
+import bcrypt from "bcryptjs";
 
 // 회원정보 수정
 export const updateUser = async (req: Request, res: Response) => {
   const userId = req.params.id;
+  const { name, password } = req.body;
 
   const user = await User.findOne({ id: userId });
   if (!user) {
-    throw new Error("사용자를 찾을 수 없습니다.");
+    return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
   }
 
-  Object.keys(updateData).forEach((key) => {
-    if (key in user) {
-      (user as any)[key] = updateData[key as keyof IUser];
-    }
-  });
+  // 이름 업데이트
+  if (name) {
+    user.name = name;
+  }
 
-  return await user.save();
+  // 비밀번호 해싱
+  if (password) {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+  }
+
+  const updatedUser = await user.save();
+
+  return res.status(200).json({
+    message: "회원정보 수정 성공",
+    user: {
+      id: updatedUser.id,
+      name: updatedUser.name,
+    },
+  });
 };
