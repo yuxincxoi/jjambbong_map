958f38aeefa8a6520786a55a66dd455575f6446b
최유진 <yjyj0774@gmail.com>
2024-12-18 21:44:04 +0900

    like place 라우트 생성

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateUser } from "../controllers/userController";
+import { updateUser, likePlace } from "../controllers/userController";
 
 const router = express.Router();
 
 // 회원정보 수정
 router.put("/:id", updateUser);
+router.post("/likeplace", likePlace);
 
 export default router;
