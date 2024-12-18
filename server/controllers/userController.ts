a3b43da134017c8b211da6f4da06f6ccc74be43f
최유진 <yjyj0774@gmail.com>
2024-12-18 21:43:44 +0900

    like place 컨트롤러 생성

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -38,3 +38,16 @@ export const updateUser = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "회원정보 수정 중 오류 발생" });
   }
 };
+
+export const likePlace = async (req: Request, res: Response) => {
+  const userId = req.params.id;
+  const { likedPlaces } = req.body;
+
+  try {
+    await User.updateOne({ id: userId }, { $set: { likePlace: likedPlaces } });
+    res.status(200).json({ message: "likePlace가 업데이트되었습니다." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "likePlace 업데이트 실패" });
+  }
+};
