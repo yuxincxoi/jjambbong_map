74b9b109a9091d30dd6115984d3858a7055106d2
최유진 <yjyj0774@gmail.com>
2024-12-23 23:54:29 +0900

    decode token jwt util 추가

--- server/utils/jwtUtils.ts
+++ server/utils/jwtUtils.ts
@@ -1,3 +1,12 @@
 import jwt from "jsonwebtoken";
 
-export const decodeToken = () => {};
+export const decodeToken = (token: string) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET || "defaultSecret") as {
+      id: string;
+    };
+  } catch (error) {
+    console.error("Invalid token:", error);
+    return null;
+  }
+};
