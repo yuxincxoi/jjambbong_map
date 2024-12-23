a0f9c00fc62a0fe6d4020b02bad184f6cd0c7439
최유진 <yjyj0774@gmail.com>
2024-12-24 00:25:03 +0900

    미들웨어 적용

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { updateUser, likePlace } from "../controllers/userController";
+import { authenticateUser } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 // 회원정보 수정
-router.put("/:id", updateUser);
-router.post("/likeplace", likePlace);
+router.put("/:id", authenticateUser, updateUser);
+router.post("/likeplace", authenticateUser, likePlace);
 
 export default router;
