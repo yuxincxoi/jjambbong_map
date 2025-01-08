b91ef290f95b67e07e54e5db6d9b427551c40472
최유진 <yjyj0774@gmail.com>
2025-01-08 14:40:28 +0900

    로그인 정보 컴포넌트 생성

---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,9 @@
+const UserInfo: React.FC = () => {
+  return (
+    <>
+      <div>안녕하세요, 김철수님</div>
+    </>
+  );
+};
+
+export default UserInfo;
--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,6 +1,7 @@
 import Map from "../components/main/Map";
 import Title from "../components/Title";
 import PlaceListTable from "../components/main/placeListTable/PlaceListTable";
+import UserInfo from "../components/UserInfo";
 import { Link } from "react-router-dom";
 
 const MainPage: React.FC = () => {
@@ -9,6 +10,7 @@ const MainPage: React.FC = () => {
       <Title />
       <Map />
       <PlaceListTable />
+      <UserInfo />
       <Link to="/">
         <button className="px-2 py-1 bg-main-color text-white rounded mr-2 hover:border-main-color hover:text-main-color hover:bg-white">
           Logout
