e9458111726b1fad8ec522e7fa9ef202c43903f3
최유진 <yjyj0774@gmail.com>
2025-01-17 15:28:59 +0900

    nav 틀 잡기

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,10 @@
 const Nav: React.FC = () => {
   return (
     <>
-      <div></div>
+      <div className="flex sticky">
+        <div className="w-16 h-16 mt-64 pr-2 bg-black"></div>
+        <div className="w-40 h-80 rounded-lg border border-stone-300"></div>
+      </div>
     </>
   );
 };
