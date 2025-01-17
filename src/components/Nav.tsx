c358c303ea108bda867744b37dc8f64ee5dc6cac
최유진 <yjyj0774@gmail.com>
2025-01-17 15:30:17 +0900

    mouse enter, leave 이벤트 설정

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const Nav: React.FC = () => {
+  const [isHidden, setIsHidden] = useState(true);
+
+  const handleHover = () => {
+    setIsHidden((prev) => !prev);
+  };
+
   return (
     <>
       <div className="flex sticky">
-        <div className="w-16 h-16 mt-64 pr-2 bg-black"></div>
-        <div className="w-40 h-80 rounded-lg border border-stone-300"></div>
+        <div
+          className="w-16 h-16 mt-64 pr-2 bg-black"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHover}
+        ></div>
+        <div
+          className={`w-40 h-80 rounded-lg border border-stone-300 ${
+            isHidden ? "hidden" : "block"
+          }`}
+          onMouseLeave={handleHover}
+        ></div>
       </div>
     </>
   );
