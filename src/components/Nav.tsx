37b7b6a5ab6c04c900ba2703e0769fb7beeec7e5
최유진 <yjyj0774@gmail.com>
2025-01-17 18:47:07 +0900

    navigation 이미지 삽입

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,6 +4,11 @@ import Button from "../components/Button";
 import UserInfo from "./UserInfo";
 
 const Nav: React.FC = () => {
+  const navBtn = "url('./img/navBtn.png')";
+  const homeBtn = "url('./img/homeBtn.png')";
+  const myPageBtn = "url('./img/myPageBtn.png')";
+  const likePageBtn = "url('./img/emptyHeart.png')";
+
   const [isHidden, setIsHidden] = useState(true);
 
   const handleHover = () => {
@@ -14,7 +19,8 @@ const Nav: React.FC = () => {
     <>
       <div className="flex sticky">
         <div
-          className="w-16 h-16 mt-64 pr-2 bg-black"
+          className="w-24 h-24 mt-64 pr-2 bg-cover bg-no-repeat"
+          style={{ backgroundImage: navBtn }}
           onMouseEnter={handleHover}
           onMouseLeave={handleHover}
         ></div>
@@ -28,8 +34,12 @@ const Nav: React.FC = () => {
             <UserInfo />
             <hr />
           </div>
-          <div>
-            <div>
+          <div className="ml-2 mt-3">
+            <div className="flex mx-auto mt-1">
+              <div
+                className="w-7 h-7 mt-1 mr-2 ml-2 mb-1 bg-cover bg-no-repeat"
+                style={{ backgroundImage: homeBtn }}
+              ></div>
               <Link to="/mainPage">
                 <Button
                   buttonName="Home"
@@ -37,7 +47,11 @@ const Nav: React.FC = () => {
                 />
               </Link>
             </div>
-            <div>
+            <div className="flex mx-auto mt-1">
+              <div
+                className="w-7 h-7 mt-1 mr-2 ml-2 mb-1 bg-cover bg-no-repeat"
+                style={{ backgroundImage: likePageBtn }}
+              ></div>
               <Link to="/likePage">
                 <Button
                   buttonName="Like"
@@ -45,7 +59,11 @@ const Nav: React.FC = () => {
                 />
               </Link>
             </div>
-            <div>
+            <div className="flex mx-auto mt-1">
+              <div
+                className="w-7 h-7 mt-1 mr-2 ml-2 mb-1 bg-cover bg-no-repeat"
+                style={{ backgroundImage: myPageBtn }}
+              ></div>
               <Link to="/myPage">
                 <Button
                   buttonName="My page"
