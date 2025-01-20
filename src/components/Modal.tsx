c968eac640514653cad624a7c7971a32d45be812
최유진 <yjyj0774@gmail.com>
2025-01-20 18:27:08 +0900

    alert message 글자 크기 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,7 +6,7 @@ export const Modal: React.FC<ModalProps> = ({ message }) => {
     <>
       <div className="fixed inset-0 flex items-center justify-center bg-neutral-400 bg-opacity-50 backdrop-blur-sm">
         <div className="w-[400px] h-[250px] bg-white rounded-xl">
-          <div className="text-center pt-16 h-32">{message}</div>
+          <div className="text-center pt-16 h-32 text-lg">{message}</div>
           <div className="mt-8">
             <Button buttonName="확인" />
           </div>
@@ -24,7 +24,7 @@ export const CloseModal: React.FC<ModalProps> = ({ message }) => {
           <div className="text-right mr-4 mt-2 font-thin text-lg cursor-pointer">
             X
           </div>
-          <div className="h-[120px] flex items-center justify-center">
+          <div className="h-[120px] flex items-center justify-center text-lg">
             {message}
           </div>
         </div>
@@ -38,7 +38,7 @@ export const ConfirmModal: React.FC<ModalProps> = ({ message }) => {
     <>
       <div className="fixed inset-0 flex items-center justify-center bg-neutral-400 bg-opacity-50 backdrop-blur-sm">
         <div className="w-[400px] h-[250px] bg-white rounded-xl">
-          <div className="text-center pt-16 h-32">{message}</div>
+          <div className="text-center pt-16 h-32 text-lg">{message}</div>
           <div className="mt-8 flex justify-evenly">
             <Button buttonName="확인" />
             <Button buttonName="취소" />
