f0c834bd91b9787e094c7176a6eb14419b4d1b35
최유진 <yjyj0774@gmail.com>
2025-01-20 17:08:12 +0900

    Modal 컴포넌트 기본 디자인 설정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,20 @@
-const Modal: React.FC = () => {
+import ModalProps from "../interfaces/components/Modal.interface";
+import Button from "./Button";
+
+const Modal: React.FC<ModalProps> = ({ message }) => {
   return (
     <>
-      <div></div>
+      <div className="fixed inset-0 flex items-center justify-center bg-neutral-400 bg-opacity-50 backdrop-blur-sm">
+        <div className="w-[400px] h-[250px] bg-white rounded-xl">
+          <div className="text-right mr-4 mt-2 font-thin text-lg cursor-pointer">
+            X
+          </div>
+          <div className="text-center pt-8 h-32">{message}</div>
+          <div className="h-20">
+            <Button buttonName="확인" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
