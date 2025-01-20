4611b53e53b88a348840f5bdffab73b930e3be6a
최유진 <yjyj0774@gmail.com>
2025-01-20 18:05:41 +0900

    모달 디자인 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,11 +6,8 @@ export const Modal: React.FC<ModalProps> = ({ message }) => {
     <>
       <div className="fixed inset-0 flex items-center justify-center bg-neutral-400 bg-opacity-50 backdrop-blur-sm">
         <div className="w-[400px] h-[250px] bg-white rounded-xl">
-          <div className="text-right mr-4 mt-2 font-thin text-lg cursor-pointer">
-            X
-          </div>
-          <div className="text-center pt-8 h-32">{message}</div>
-          <div className="h-20">
+          <div className="text-center pt-16 h-32">{message}</div>
+          <div className="mt-8">
             <Button buttonName="확인" />
           </div>
         </div>
