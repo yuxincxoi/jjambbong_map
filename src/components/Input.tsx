9ca152e8303182aa60a294d854c88a4dedf76b89
최유진 <yjyj0774@gmail.com>
2025-01-30 16:31:51 +0900

    input 컴포넌트 디자인 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,16 +11,23 @@ const Input: React.FC<InputProps> = ({
 }) => {
   return (
     <>
-      <div className="flex justify-center mt-2">
-        <input
-          type={type}
-          placeholder={placeholder}
-          name={name}
-          value={value}
-          disabled={disabled}
-          onChange={onChange}
-          className={`w-60 h-10 px-4 border-[1px] rounded-md hover:border-main-color focus:outline-none focus:border-main-color font-extralight ${className}`}
-        />
+      <div className="flex justify-center items-center">
+        <div className="w-60">
+          <div className="relative h-14 leading-[40px]">
+            <input
+              aria-label="input"
+              type={type}
+              name={name}
+              value={value}
+              disabled={disabled}
+              onChange={onChange}
+              className={`absolute w-full outline-none px-4 leading-[40px] border-[1px] bg-transparent transition duration-100 z-1 rounded-md hover:border-main-color focus:border-main-color font-extralight peer ${className}`}
+            />
+            <div className="absolute h-5 text-md mx-4 my-1 transition duration-200 text-gray-400 font-extralight bg-white leading-[35px] z-2 peer-focus:text-main-color peer-focus:leading-[30px] peer-focus:transform peer-focus:-translate-y-5 peer-focus:scale-[0.88]">
+              {placeholder}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
