bcd566659455941a1921123cbaf8225535d23f83
최유진 <yjyj0774@gmail.com>
2025-01-30 16:52:58 +0900

    focus 상태에 따른 placeholder 디자인 변경 설정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import InputProps from "../interfaces/components/Input.interface";
 
 const Input: React.FC<InputProps> = ({
@@ -9,6 +10,10 @@ const Input: React.FC<InputProps> = ({
   disabled,
   onChange,
 }) => {
+  const [isFocused, setIsFocused] = useState(true);
+
+  const handleFocus = () => setIsFocused(true);
+
   return (
     <>
       <div className="flex justify-center items-center">
@@ -21,10 +26,11 @@ const Input: React.FC<InputProps> = ({
               value={value}
               disabled={disabled}
               onChange={onChange}
+              onFocus={handleFocus}
               className={`absolute w-full outline-none px-4 leading-[40px] border-[1px] bg-transparent transition duration-100 z-1 rounded-md hover:border-main-color focus:border-main-color font-extralight peer ${className}`}
             />
             <div className="absolute h-5 text-md mx-4 my-1 transition duration-200 text-gray-400 font-extralight bg-white leading-[35px] z-2 peer-focus:text-main-color peer-focus:leading-[30px] peer-focus:transform peer-focus:-translate-y-5 peer-focus:scale-[0.88]">
-              {placeholder}
+              {value || !isFocused ? "" : placeholder}
             </div>
           </div>
         </div>
