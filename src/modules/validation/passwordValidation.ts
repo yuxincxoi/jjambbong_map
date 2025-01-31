b4c5e281cc6956ee055011a38f527a04101634c8
최유진 <yjyj0774@gmail.com>
2025-01-31 16:00:58 +0900

    password 유효성 검사 코드 생성

---
+++ src/modules/validation/passwordValidation.ts
@@ -0,0 +1,5 @@
+export const validatePassword = (password: string) => {
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordRegex.test(password);
+};
