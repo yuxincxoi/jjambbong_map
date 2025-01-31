660c8b687308c9106f71d00194940c40da41488d
최유진 <yjyj0774@gmail.com>
2025-01-31 16:53:28 +0900

    input 컴포넌트 디자인 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -17,8 +17,8 @@ const Input: React.FC<InputProps> = ({
   return (
     <>
       <div className="flex justify-center items-center">
-        <div className="w-60">
-          <div className="relative h-14 leading-[40px]">
+        <div className={`w-80 ${className}`}>
+          <div className="relative h-10 leading-[40px]">
             <input
               aria-label="input"
               type={type}
@@ -27,7 +27,7 @@ const Input: React.FC<InputProps> = ({
               disabled={disabled}
               onChange={onChange}
               onFocus={handleFocus}
-              className={`absolute w-full outline-none px-4 leading-[40px] border-[1px] bg-transparent transition duration-100 z-1 rounded-md hover:border-main-color focus:border-main-color font-extralight peer ${className}`}
+              className="absolute w-full outline-none px-4 leading-[40px] border-[1px] bg-transparent transition duration-100 z-1 rounded-md hover:border-main-color focus:border-main-color font-extralight peer"
             />
             <div className="absolute h-5 text-md mx-4 my-1 transition duration-200 text-gray-400 font-extralight bg-white leading-[35px] z-2 peer-focus:text-main-color peer-focus:leading-[30px] peer-focus:transform peer-focus:-translate-y-5 peer-focus:scale-[0.88]">
               {value || !isFocused ? "" : placeholder}
