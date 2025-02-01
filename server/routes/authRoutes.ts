5f069f232e121c56bf9083a87771819bac0a21f3
최유진 <yjyj0774@gmail.com>
2025-02-01 17:48:46 +0900

    인증 상태 관리 라우트 생성

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -4,11 +4,13 @@ import {
   loginUser,
   logoutUser,
 } from "../controllers/authController";
+import { authenticateUser } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.get("/me", authenticateUser);
 
 export default router;
