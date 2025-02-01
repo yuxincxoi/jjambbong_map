951d15629983a512b57ef4df39bed2bc122f44c4
최유진 <yjyj0774@gmail.com>
2025-02-01 17:45:28 +0900

    공개 라우트와 보호된 라우트 구분

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,41 @@ import SignupPage from "./pages/signupPage";
 import MainPage from "./pages/mainPage";
 import LikePage from "./pages/likePage";
 import MyPage from "./pages/myPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 공개 라우트 */}
         <Route path="/" element={<IndexPage />} />
         <Route path="/signupPage" element={<SignupPage />} />
-        <Route path="/mainPage" element={<MainPage />} />
-        <Route path="/likePage" element={<LikePage />} />
-        <Route path="/myPage" element={<MyPage />} />
+
+        {/* 보호된 라우트 */}
+        <Route
+          path="/mainPage"
+          element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/likePage"
+          element={
+            <ProtectedRoute>
+              <LikePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/myPage"
+          element={
+            <ProtectedRoute>
+              <MyPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
