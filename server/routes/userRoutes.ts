23fe056fd0b47a2a0ef6e5e8e2ca93daed18db46
최유진 <yjyj0774@gmail.com>
2025-02-01 16:45:21 +0900

    id 중복 확인 라우터 생성

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,5 +1,10 @@
 import express from "express";
-import { updateUser, likePlace, userInfo } from "../controllers/userController";
+import {
+  updateUser,
+  likePlace,
+  userInfo,
+  checkDuplication,
+} from "../controllers/userController";
 import { authenticateUser } from "../middlewares/auth.middleware";
 
 const router = express.Router();
@@ -8,5 +13,6 @@ const router = express.Router();
 router.put("/:id", authenticateUser, updateUser);
 router.post("/likeplace", authenticateUser, likePlace);
 router.get("/me", authenticateUser, userInfo);
+router.get("/email-check", checkDuplication);
 
 export default router;
