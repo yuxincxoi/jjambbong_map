ce96fe1cedf26d29223d14cbc100a3bc53e4c868
최유진 <yjyj0774@gmail.com>
2025-02-05 19:46:09 +0900

    EachPlace interface 수정

--- src/interfaces/components/main/placeListTable/eachPlace/EachPlace.interface.ts
+++ src/interfaces/components/main/placeListTable/eachPlace/EachPlace.interface.ts
@@ -2,5 +2,6 @@ import Place from "../Place.interface";
 
 export default interface EachPlaceProps {
   place: Place;
+  isClicked: boolean;
   onLikeToggle: (place: Place, isHeartOn: boolean) => void;
 }
