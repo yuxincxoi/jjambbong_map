691c919c29be57d67f197d924a3e5af445b6bd4d
최유진 <yjyj0774@gmail.com>
2025-02-17 18:05:33 +0900

    nav toggle 가운데 정렬

--- src/components/NavToggle.tsx
+++ src/components/NavToggle.tsx
@@ -3,7 +3,11 @@ import { NavToggleProps } from "../interfaces/components/NavToggle.interface";
 
 const NavToggle: React.FC<NavToggleProps> = ({ onToggle, isNavVisible }) => {
   return (
-    <div className="lg:hidden z-50">
+    <div
+      className={`lg:hidden z-50 flex justify-center ${
+        isNavVisible ? "" : "absolute bottom-24 left-1/2 -translate-x-1/2"
+      }`}
+    >
       <Button
         onClick={onToggle}
         buttonName=""
