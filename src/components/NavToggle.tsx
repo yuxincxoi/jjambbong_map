3f04c020c2a591fb83b05e1e9de8a83ab73e0293
최유진 <yjyj0774@gmail.com>
2025-02-17 18:07:53 +0900

    nav toggle bg 설정

--- src/components/NavToggle.tsx
+++ src/components/NavToggle.tsx
@@ -1,6 +1,9 @@
 import { NavToggleProps } from "../interfaces/components/NavToggle.interface";
 
 const NavToggle: React.FC<NavToggleProps> = ({ onToggle, isNavVisible }) => {
+  const upArrow = "url('./img/upArrow.png')";
+  const downArrow = "url('./img/downArrow.png')";
+
   return (
     <div
       className={`lg:hidden z-50 flex justify-center ${
@@ -10,6 +13,7 @@ const NavToggle: React.FC<NavToggleProps> = ({ onToggle, isNavVisible }) => {
       <div
         onClick={onToggle}
         className="bg-white px-4 py-3 rounded-t-lg border-[1px] border-b-0 text-gray-700 hover:bg-gray-50 cursor-pointer"
+        style={{ backgroundImage: isNavVisible ? downArrow : upArrow }}
       ></div>
     </div>
   );
